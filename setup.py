841841a5df796cf4c8df68b17eb57a25dc0416b7
Kawsar Ahmmed <kawsar07ahmmed0712@gmail.com>
2026-04-07 09:30:43 +0600

    setup complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages 
+setup(
+    name="medical_chatbot",
+    version="0.1",
+    author="Kawsar Ahmmed",
+    author_email="kawsar07ahmmed0712@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
